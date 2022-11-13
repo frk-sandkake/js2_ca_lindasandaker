9af3b021fc291abe559f8d2fdf17640a7607e679
Linda Sandaker <frk.sandkake@gmail.com>
2022-11-13 18:39:22 +0100

    Revert "Revert "Install eslint airbnb""

--- js/settings/api.js
+++ js/settings/api.js
@@ -1,23 +1,23 @@
-import { getUserNameStorage } from "../utils/storage";
+import { getUserNameStorage } from '../utils/storage';
+
 const userName = getUserNameStorage();
 
-const API_URL_BASE = "https://nf-api.onrender.com/";
+const API_URL_BASE = 'https://nf-api.onrender.com/';
 
 /* AUTH */
-const USER_SIGNUP_URL = API_URL_BASE + "api/v1/social/auth/register";
-const USER_LOGIN_URL = API_URL_BASE + "api/v1/social/auth/login";
+const USER_SIGNUP_URL = `${API_URL_BASE}api/v1/social/auth/register`;
+const USER_LOGIN_URL = `${API_URL_BASE}api/v1/social/auth/login`;
 
 /* POSTS */
-const CREATE_POST_URL = API_URL_BASE + "api/v1/social/posts";
-const EDIT_POST_URL = API_URL_BASE + "api/v1/social/posts";
-const DELETE_POST_URL = API_URL_BASE + "api/v1/social/posts";
-const GET_POSTS_URL = API_URL_BASE + "api/v1/social/posts";
-const SORT_DESC_POSTS_URL = API_URL_BASE + "api/v1/social/posts?sort=created&sortOrder=desc"
-const SORT_ASC_POSTS_URL = API_URL_BASE + "api/v1/social/posts?sort=created&sortOrder=asc"
-const GET_POST_ID_URL = API_URL_BASE + "api/v1/social/posts";
+const CREATE_POST_URL = `${API_URL_BASE}api/v1/social/posts`;
+const EDIT_POST_URL = `${API_URL_BASE}api/v1/social/posts`;
+const DELETE_POST_URL = `${API_URL_BASE}api/v1/social/posts`;
+const GET_POSTS_URL = `${API_URL_BASE}api/v1/social/posts`;
+const SORT_DESC_POSTS_URL = `${API_URL_BASE}api/v1/social/posts?sort=created&sortOrder=desc`;
+const SORT_ASC_POSTS_URL = `${API_URL_BASE}api/v1/social/posts?sort=created&sortOrder=asc`;
+const GET_POST_ID_URL = `${API_URL_BASE}api/v1/social/posts`;
 // const GET_USER_PROFILE_URL = API_URL_BASE + `api/v1/social/profiles/${userName}`;
-const GET_USER_POSTS_URL =
-  API_URL_BASE + `api/v1/social/profiles/${userName}?_posts=true`;
+const GET_USER_POSTS_URL = `${API_URL_BASE}api/v1/social/profiles/${userName}?_posts=true`;
 
 export {
   API_URL_BASE,
@@ -30,6 +30,6 @@ export {
   SORT_DESC_POSTS_URL,
   SORT_ASC_POSTS_URL,
   GET_POST_ID_URL,
- // GET_USER_PROFILE_URL,
+  // GET_USER_PROFILE_URL,
   GET_USER_POSTS_URL,
 };
